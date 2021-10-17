c21a580538b0937d7a38c0d5445ec7b82b3abdaf
kouki <kouworld0123@gmail.com>
2021-10-17 13:44:47 +0000

    add unique constraint to username
    Signed-off-by: kouki <kouworld0123@gmail.com>

--- model/user.go
+++ model/user.go
@@ -4,6 +4,6 @@ import "github.com/jinzhu/gorm"
 
 type User struct {
 	gorm.Model
-	UserID   string `json:"user_id"`
+	UserID   string `json:"user_id" gorm:"unique"`
 	Password string `json:"password"`
 }
